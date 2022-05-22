c6804f2dff8fdc2f35cf84761ea8b7b43d71fa9b
Matokuun <matiasadorno14@gmail.com>
2022-05-21 22:39:38 -0300

    agregado el cargar de c/ vacuna, faltan turnos

--- src/components/Cargar.js
+++ src/components/Cargar.js
@@ -4,8 +4,18 @@ import { createUserData } from "../firebase";
 import { useAuth } from "../context/AuthContext";
 
 const Cargar = () => {
+    //HU: Cargar info
     const [ zona, setZona ] = useState( '' ) //valor x defecto
     const [ factorRiesgo, setFactorRiesgo] = useState(false) //valor x defecto
+    //HU: Cargar info vacuna COVID
+    const [ cantidadDosisCOVID, setCantidadDosisCOVID ] = useState( '' )
+    //HU: Cargar info vacuna GRIPE
+    const [ tieneVacuna, setTieneVacuna ] = useState( '' )
+    const [ fechaVacGripe, setFechaVacGripe ] = useState( '' )
+    //HU: Cargar info vacuna FIEBRE AMARILLA
+    const [ dosisAmarilla, setDosisAmarilla ] = useState( '' )
+    const [ añoDosisAmarilla, setAñoDosisAmarilla ] =useState( '' )
+
     const navigate = useNavigate()
     const {user} = useAuth();
     const store= async (e) => { //funcion para almacenar
@@ -17,7 +27,7 @@ const Cargar = () => {
       <div className='container'>
           <div className='row'>
               <div className='col'>
-                  <h1>Cargar Datos Importantes</h1>
+                  <h1> Datos Importantes </h1>
                   <form onSubmit={store}>
                   <div className='mb-3'>
                           <label className='form-label'>Vacunatorio</label>
@@ -39,7 +49,64 @@ const Cargar = () => {
                               required
                           />    
                       </div>
-                      <button type='submit' className='btn btn-primary'>store</button>
+                      <h1> Datos Vacuna COVID-19 </h1>
+                      <div className='mb-3'>
+                          <label className='form-label'>Cantidad de dosis de COVID-19 recibidas</label>
+                          <input
+                              value={cantidadDosisCOVID}
+                              onChange={(e) => setCantidadDosisCOVID(e.target.value)}
+                              type="number"
+                              className='form-control'
+                              required
+                              min={0}
+                              max={2}
+                          />    
+                      </div>  
+                      <h1> Datos Vacuna GRIPE </h1>
+                      <div className='mb-3'>
+                          <label className='form-label'>¿Posee la vacuna para la gripe?</label>
+                          <input
+                              value={tieneVacuna}
+                              onChange={(e) => setTieneVacuna(e.target.value)}
+                              type="text"
+                              className='form-control'
+                              required
+                          />    
+                      </div>
+                      <div className='mb-3'>
+                          <label className='form-label'>¿En qué fecha se la dió?</label>
+                          <input
+                              value={fechaVacGripe}
+                              onChange={(e) => setFechaVacGripe(e.target.value)}
+                              type="date"
+                              className='form-control'
+                              required
+                          />    
+                      </div> 
+                      <h1> Datos Vacuna FIEBRE AMARILLA </h1>
+                      <div className='mb-3'>
+                          <label className='form-label'>¿Tiene la vacuna de la fiebre amarilla?</label>
+                          <input
+                              value={dosisAmarilla}
+                              onChange={(e) => setDosisAmarilla(e.target.value)}
+                              type="text"
+                              className='form-control'
+                              required
+                          />    
+                      </div>
+                      <div className='mb-3'>
+                          <label className='form-label'>Año en que se la aplicó</label>
+                          <input
+                              value={añoDosisAmarilla}
+                              onChange={(e) => setAñoDosisAmarilla(e.target.value)}
+                              type="number"
+                              className='form-control'
+                              required
+                              min={1500}
+                              max={2022}
+                          />    
+                      </div>
+                      <button type='submit' className='btn btn-primary'>guarda los datos pa</button>
                   </form>
               </div>
           </div>
