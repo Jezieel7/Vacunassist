6453573e23d7803e800c132a3a601a92ca96ea62
JezielPaladino <leandropaladino15@gmail.com>
2022-05-30 20:05:53 -0300

    Se terminaron las funcionalidades de inicio y registro de vacunador

--- src/components/RegisterOnlyVaccinator.js
+++ src/components/RegisterOnlyVaccinator.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { Alert } from "./Alert";
 import { createUserDocumentVaccinator } from "../firebase";
-export function Register(){
+export function RegisterOnlyVaccinator(){
     const [user, setUser] = useState({
         email: '',
         password: '',
