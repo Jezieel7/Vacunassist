800b37f228803fc4cbca968b1b3b8b4057692f2f
Matokuun <matiasadorno14@gmail.com>
2022-07-07 20:13:34 -0300

    agregado search en la busqueda de emails, factor de riesgo

--- src/components/ModificarFactorRiesgo.js
+++ src/components/ModificarFactorRiesgo.js
@@ -1,6 +1,6 @@
 import { useAuth } from "../context/AuthContext";
-import React, { useState } from 'react';
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import React, { useState, useEffect } from 'react';
+import { doc, getDoc, updateDoc, collection, query, where, getDocs} from "firebase/firestore";
 import { db } from "../firebase";
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
@@ -10,8 +10,28 @@ export default function ModificarFactorRiesgo(){
     const { logout, loading } = useAuth();
     const [ riesgo, setRiesgo] = useState('');
     const [ user, setUser ] = useState({
-        email: ''
+        email: 'otro'
     });
+    const [personas, setPersonas] = useState( [] );
+    const [mati, setMati] = useState( 0 );
+    
+    const getListadoPersonas = async (string) => {
+        const listado = query(collection(db, string), where("user.vaccinator", "!=", "true"));
+        var arr1= [];
+        const querySnapshot1 = await getDocs(listado);
+        querySnapshot1.forEach((doc) => {
+            if(doc.data().user.email !== "lautaro@gmail.com" && doc.data().user.email !== "mativacunador@gmail.com" && doc.data().user.email !== "jezielvacunador@gmail.com" && doc.data().user.email !== "dainavacunadora@gmail.com")
+                arr1.push(doc.data());
+        });
+        arr1 = [...new Set(arr1)];
+        setPersonas(arr1); //en personas voy a guardar los mails
+        setMati(1);
+    }
+
+    useEffect( () => {
+        getListadoPersonas("Persona");
+        // eslint-disable-next-time
+    }, [])
 
     const handleChange = ({target: {name, value}}) => {
         if(name === "email"){
@@ -24,15 +44,25 @@ export default function ModificarFactorRiesgo(){
     const submitRiesgo = async (e) => {
         e.preventDefault();
         setError('');
+        console.log(user.email)
         try {
+            if(user.email=="listo"){
+                MySwal.fire(`Ya actualizó el factor de riesgo, actualize la pagina para ver los cambios, o seleccione otro`);
+                throw error;
+            }
+            if(user.email=="otro"){
+                MySwal.fire(`Ingrese un email`);
+                throw error;
+            }
             const docRef = doc(db,`Persona/${user.email}`);
             const docSnap = await getDoc(docRef); 
             if (docSnap.exists()) {
                 await updateDoc(docRef, {"user.riskFactor": riesgo});
                 if(riesgo == "true")
-                    MySwal.fire("Por favor, recuerdele a esta persona que si tiene turnos puede cancelarlos y pedir otros con mayor prioridad");
+                    MySwal.fire("Se registro el nuevo factor de riesgo. Por favor, recuerdele a esta persona que si tiene turnos puede cancelarlos y pedir otros con mayor prioridad");
                 else
                     MySwal.fire(`Se registro el nuevo factor de riesgo`);
+                user.email="listo"
             } else {
                 MySwal.fire(`El email ingresado no pertenece a un usuario del sistema`);
                 throw error;
@@ -52,9 +82,16 @@ export default function ModificarFactorRiesgo(){
                     </div>
                     <h1>Modificar factor de riesgo</h1>
                     <form onSubmit={submitRiesgo}>
-                        <div className='mb-3'>
-                            <label className='form-label'>Email del usuario: </label>
-                            <input type="email" className='form-control' name="email" value={user.email} onChange={handleChange}/>     
+                        <div className='mb-3'> 
+                            <label className='form-label'>Email (entre parentesis si tiene riesgo actualmente): </label>
+                            <input type="search" name="email" list="personas" onChange={handleChange} inlist="personas"></input>
+                            <datalist id="personas">
+                            {mati == 1 ?
+                                personas.map( (persona) => (
+                                    <option type="text" className='form-control' name="email" value={persona.user.email}>{persona.user.email}{persona.user.riskFactor=="true"?"(si)":"(no)"}</option>
+                                )) 
+                            : ""}
+                            </datalist>
                         </div>
                         <div className='mb-3'> 
                             <label className='form-label' htmlFor="riskFactor">Actualizar riesgo: </label>
@@ -68,3 +105,13 @@ export default function ModificarFactorRiesgo(){
         </div>
     )
 }
+/*
+    {mati == 1 ?(
+                            <select type="search" name="email" onChange={handleChange}>
+                                <option type="email" className='form-control' name="email" value={"mati"}>Selecciona un email</option>
+                                {personas.map( (persona) => (
+                                    <option type="email" className='form-control' name="email" value={persona.user.email}>{persona.user.email}{persona.user.riskFactor=="true"?"(si)":"(no)"}</option>
+                                ))}  
+                            </select>
+                        ): ""}
+*/
